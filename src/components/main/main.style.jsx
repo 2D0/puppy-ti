0dfd93a4f3e4c0fbc158107772a81e9c20d54fe5
2D0 <lo7733ve@naver.com>
2023-02-14 22:17:53 +0900

    style: main 스타일 작업 중

---
+++ src/components/main/main.style.jsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+import { BoxCenterRow } from '@/style/CommonContents.jsx';
+
+// **************** 컨텐츠 ****************
+//메인 이미지
+export const MainTopImg = styled.img`
+    width: 59.4%;
+    height: auto;
+    z-index: 10;
+    ${BoxCenterRow}
+    top: 19vh;
+`;
