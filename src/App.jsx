42c06afc0815d62d233c46a9bf7937902a3ed603
2D0 <lo7733ve@naver.com>
2023-02-14 12:24:13 +0900

    feat: 라우터 연결

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { Wrap } from '@/style/CommonContents.jsx';
+import { Main, Check, Inquiry } from '@/components/pages';
 
 function App() {
-    return <>app</>;
+    return (
+        <Wrap>
+            <Routes>
+                <Route path="/" element={<Main />}></Route>
+                <Route path="/check" element={<Check />}></Route>
+                <Route path="/inquiry" element={<Inquiry />}></Route>
+            </Routes>
+        </Wrap>
+    );
 }
 
 export default App;
