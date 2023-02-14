a453dfd68e046a32fbb2a3e33e912472ff370598
2D0 <lo7733ve@naver.com>
2023-02-14 22:17:01 +0900

    feat: 마크업 작성

---
+++ src/components/pages/SourceLicense.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import BackGround from '@components/common/BackGround.jsx';
+
+const SourceLicense = () => {
+    return (
+        <div>
+            source license
+            <BackGround></BackGround>
+        </div>
+    );
+};
+export default SourceLicense;
---
+++ src/components/pages/TeamMember.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import BackGround from '@components/common/BackGround.jsx';
+
+const TeamMember = () => {
+    return (
+        <div>
+            teamMember
+            <BackGround></BackGround>
+        </div>
+    );
+};
+export default TeamMember;
