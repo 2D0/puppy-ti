8428ffab2035c396f7233e453377c61f4d9dedfa
2D0 <lo7733ve@naver.com>
2023-02-14 22:18:38 +0900

    feat: 마크업 작성

--- src/components/pages/Inquiry.jsx
+++ src/components/pages/Inquiry.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
+import BackGround from '@components/common/BackGround.jsx';
 
 const Inquiry = () => {
-    return <div>inquiry test</div>;
+    return (
+        <div>
+            inquiry
+            <BackGround></BackGround>
+        </div>
+    );
 };
 export default Inquiry;
