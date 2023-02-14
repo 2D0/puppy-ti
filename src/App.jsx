bd9acd04d419066dc81e8be3aecdef1b3af34d8a
2D0 <lo7733ve@naver.com>
2023-02-14 22:14:16 +0900

    feat: 라우터 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { Wrap } from '@/style/CommonContents.jsx';
-import { Main, Check, Inquiry } from '@/components/pages';
+import { Main, Check, Result, TeamMember, Inquiry, SourceLicense } from '@/components/pages';
 
 function App() {
     return (
@@ -9,7 +9,10 @@ function App() {
             <Routes>
                 <Route path="/" element={<Main />}></Route>
                 <Route path="/check" element={<Check />}></Route>
+                <Route path="/result" element={<Result />}></Route>
+                <Route path="/team_member" element={<TeamMember />}></Route>
                 <Route path="/inquiry" element={<Inquiry />}></Route>
+                <Route path="/source_license" element={<SourceLicense />}></Route>
             </Routes>
         </Wrap>
     );
