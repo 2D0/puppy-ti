236d49730b0e69b500fdb917ae63ab494d793383
2D0 <lo7733ve@naver.com>
2023-02-15 12:45:41 +0900

    feat: 백그라운드 컴포넌트 App.jsx로 옮김

--- src/components/pages/Inquiry.jsx
+++ src/components/pages/Inquiry.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import BackGround from '@components/common/BackGround.jsx';
 
 const Inquiry = () => {
     return (
         <div>
             inquiry
-            <BackGround></BackGround>
         </div>
     );
 };
