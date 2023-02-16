ff9cc31d62568c8935a1430b380b0e179f523494
2D0 <lo7733ve@naver.com>
2023-02-16 13:18:55 +0900

    feat: 푸터 마크업 작업 중

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -1,6 +1,24 @@
 import React from 'react';
 
+//스타일
+import * as C from '@/style/CommonContents';
+
 const Footer = () => {
-    return <>footer</>;
+    return <C.Footer>
+        <C.FooterInfo>
+            <C.FooterInfoList>
+                <C.FooterInfoLink to='/team_member'>팀원 소개</C.FooterInfoLink>
+            </C.FooterInfoList>
+            <C.FooterInfoList>
+                <C.FooterInfoLink to='/inquiry'>개발 문의</C.FooterInfoLink>
+            </C.FooterInfoList>
+            <C.FooterInfoList>
+                <C.FooterInfoLink to='/source_license'>오픈 소스 라이센스</C.FooterInfoLink>
+            </C.FooterInfoList>
+        </C.FooterInfo>
+        <C.FooterCopy>
+            ⓒ 2023. MAKETREE All rights reserved.
+        </C.FooterCopy>
+    </C.Footer>;
 };
 export default Footer;
