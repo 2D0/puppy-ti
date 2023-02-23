5924c235529fc69339c498d61376e0c021ab46de
2D0 <lo7733ve@naver.com>
2023-02-24 01:30:00 +0900

    chore: 작업내역 없음

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -4,21 +4,21 @@ import React from 'react';
 import * as C from '@/style/CommonContents';
 
 const Footer = () => {
-    return <C.Footer>
-        <C.FooterInfo>
-            <C.FooterInfoList>
-                <C.FooterInfoLink to='/team_member'>팀원 소개</C.FooterInfoLink>
-            </C.FooterInfoList>
-            <C.FooterInfoList>
-                <C.FooterInfoLink to='/inquiry'>개발 문의</C.FooterInfoLink>
-            </C.FooterInfoList>
-            <C.FooterInfoList>
-                <C.FooterInfoLink to='/source_license'>오픈 소스 라이센스</C.FooterInfoLink>
-            </C.FooterInfoList>
-        </C.FooterInfo>
-        <C.FooterCopy>
-            ⓒ 2023. MAKETREE All rights reserved.
-        </C.FooterCopy>
-    </C.Footer>;
+    return (
+        <C.Footer>
+            <C.FooterInfo>
+                <C.FooterInfoList>
+                    <C.FooterInfoLink to={'/team_member'}>팀원 소개</C.FooterInfoLink>
+                </C.FooterInfoList>
+                <C.FooterInfoList>
+                    <C.FooterInfoLink to={'/inquiry'}>개발 문의</C.FooterInfoLink>
+                </C.FooterInfoList>
+                <C.FooterInfoList>
+                    <C.FooterInfoLink to={'/source_license'}>오픈 소스 라이센스</C.FooterInfoLink>
+                </C.FooterInfoList>
+            </C.FooterInfo>
+            <C.FooterCopy>ⓒ 2023. MAKETREE All rights reserved.</C.FooterCopy>
+        </C.Footer>
+    );
 };
 export default Footer;
