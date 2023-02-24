6cb30d67ef5902e114ca42c2280048c710e51e16
2D0 <lo7733ve@naver.com>
2023-02-24 22:41:22 +0900

    feat: App.jsx에서 props 받아와 강아지 이름 state input에 입력된 값으로 변경

--- src/components/pages/Main.jsx
+++ src/components/pages/Main.jsx
@@ -11,9 +11,10 @@ import * as S from '@components/main/Main.style.jsx';
 //이미지
 import { Intj } from '@/assets/img/mbti';
 
-function Main() {
+function Main({ setName }) {
     const [startActive, setStartActive] = useState(false); //버튼 활성화
     const [dogName, setDogName] = useState(''); //input value
+    const navigate = useNavigate(); //react router 페이지 핸들링하는 함수
 
     //input에 글자가 1개 이상 입력될 경우 버튼 활성화
     const onNameChange = e => {
@@ -21,7 +22,12 @@ function Main() {
         setDogName(value);
         setStartActive(value.length >= 1);
     };
-    const navigate = useNavigate(); //react router 페이지 핸들링하는 함수
+
+    //시작하기 버튼 누르면 input 값 전달 및 check 페이지로 이동
+    const passName = () => {
+        dogName !== '' && setName(dogName);
+        startActive && navigate('/check');
+    };
     return (
         <C.Content>
             <S.MainCont>
@@ -35,7 +41,7 @@ function Main() {
 
                 <S.MainStart>
                     <S.MainInput onChange={onNameChange} value={dogName} type="text" placeholder="반려견 이름을 적어주세요." />
-                    <C.CommonBtn startActive={startActive} onClick={() => startActive && navigate('/check')} type="submit">
+                    <C.CommonBtn startActive={startActive} onClick={passName} type="submit">
                         시작하기
                     </C.CommonBtn>
                 </S.MainStart>
