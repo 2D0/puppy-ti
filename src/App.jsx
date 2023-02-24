fe7d95a3d00b9af325615b012b8130e579855a22
2D0 <lo7733ve@naver.com>
2023-02-24 22:40:31 +0900

    feat: 메인에서 강아지 이름 받아올 useState 생성 후 받아 온 데이터 결과 페이지로 보내기

--- src/App.jsx
+++ src/App.jsx
@@ -9,8 +9,14 @@ import { Header, Footer, Background } from '@/components/common';
 import { Main, Check, Result, TeamMember, Inquiry, SourceLicense } from '@/components/pages';
 
 function App() {
-    const [purpleBg, setPurpleBg] = useState(true);
+    const [purpleBg, setPurpleBg] = useState(true); //보라색 배경 on off 상태
+    const [name, setName] = useState(''); //강아지 이름
 
+    //결과 페이지 프롭스 전달
+    const resultProps = {
+        setPurpleBg: setPurpleBg, //배경 On Off
+        name: name, //강아지 이름
+    };
     return (
         <C.Wrap>
             <Header setPurpleBg={setPurpleBg} />
@@ -18,9 +24,9 @@ function App() {
             {purpleBg && <Background />}
 
             <Routes>
-                <Route path="/" element={<Main />}></Route>
+                <Route path="/" element={<Main setName={setName} />}></Route>
                 <Route path="/check" element={<Check setPurpleBg={setPurpleBg} />}></Route>
-                <Route path="/result" element={<Result setPurpleBg={setPurpleBg} />}></Route>
+                <Route path="/result" element={<Result resultProps={resultProps} />}></Route>
                 <Route path="/team_member" element={<TeamMember />}></Route>
                 <Route path="/inquiry" element={<Inquiry />}></Route>
                 <Route path="/source_license" element={<SourceLicense />}></Route>
