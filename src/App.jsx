534e76ca7a18b97b39f328442622701e510ebe62
2D0 <lo7733ve@naver.com>
2023-02-26 00:24:41 +0900

    feat: 불필요한 함수,변수 정리 및 이름 변경

--- src/App.jsx
+++ src/App.jsx
@@ -9,38 +9,19 @@ import { Header, Footer, Background } from '@/components/common';
 import { Main, Check, Result, TeamMember, Inquiry, SourceLicense } from '@/components/pages';
 
 function App() {
-    const [purpleBg, setPurpleBg] = useState(true); //보라색 배경 on off 상태
+    const [bgColor, setBgColor] = useState('purple');
     const [name, setName] = useState(''); //강아지 이름
-    const [blackBack, setBlackBack] = useState(false); //100%시 검정배경
-
-    //헤더 프롭스 전달
-    const headerProps = {
-        setPurpleBg: setPurpleBg, //배경 On Off
-        blackBack: blackBack, //배경 검정
-    };
-
-    //체크 페이지 프롭스 전달
-    const checkProps = {
-        setPurpleBg: setPurpleBg, //배경 On Off
-        setBlackBack: setBlackBack, //100%시 검정배경
-    };
-
-    //결과 페이지 프롭스 전달
-    const resultProps = {
-        setPurpleBg: setPurpleBg, //배경 On Off
-        name: name, //강아지 이름
-    };
 
     return (
         <C.Wrap>
-            <Header headerProps={headerProps} />
+            <Header bgColor={bgColor} />
 
-            {purpleBg && <Background />}
+            {bgColor === 'purple' && <Background />}
 
             <Routes>
                 <Route path="/" element={<Main setName={setName} />}></Route>
-                <Route path="/check" element={<Check checkProps={checkProps} />}></Route>
-                <Route path="/result" element={<Result resultProps={resultProps} />}></Route>
+                <Route path="/check" element={<Check setBgColor={setBgColor} />}></Route>
+                <Route path="/result" element={<Result setBgColor={setBgColor} name={name} />}></Route>
                 <Route path="/team_member" element={<TeamMember />}></Route>
                 <Route path="/inquiry" element={<Inquiry />}></Route>
                 <Route path="/source_license" element={<SourceLicense />}></Route>
