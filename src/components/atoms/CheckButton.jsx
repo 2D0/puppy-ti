6f9282d8e4c83a1f40c5ea7fb2db3a22b58fd56f
2D0 <lo7733ve@naver.com>
2023-02-26 17:25:24 +0900

    feat: svgr 적용 중 (이 부분에서 에러가 낢)

--- src/components/atoms/CheckButton.jsx
+++ src/components/atoms/CheckButton.jsx
@@ -1,7 +1,20 @@
-import { Foot } from '@/assets/img/icons'; //이미지
+import { Foot } from '@/assets/img/icons';
 
-const FootPrint = () => (
-    <div>
-        <Foot />
-    </div>
-);
+svgr({
+    exportAsDefault: false,
+
+    // svgr options: https://react-svgr.com/docs/options/
+    svgrOptions: {
+        // ...
+    },
+
+    // esbuild options, to transform jsx to js
+    esbuildOptions: {
+        // ...
+    },
+
+    //  A minimatch pattern, or array of patterns, which specifies the files in the build the plugin should include. By default all svg files will be included.
+    include: '**/*.svg',
+
+    exclude: '',
+});
