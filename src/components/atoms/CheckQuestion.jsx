fbf11bccea19d2090700825fac1a0b2966093bb2
2D0 <lo7733ve@naver.com>
2023-02-26 23:09:18 +0900

    feat: check 버튼 컴포넌트 추가 후 map 적용

--- src/components/atoms/CheckQuestion.jsx
+++ src/components/atoms/CheckQuestion.jsx
@@ -1,12 +1,14 @@
-import React from 'react';
-
-//컴포넌트
-import CheckButton from '@/components/atoms/CheckButton.jsx';
+import React, { useState } from 'react';
 
 //스타일
 import * as A from '@components/atoms/atoms.style.jsx';
 
 const CheckQuestion = () => {
+    const foot = [1, 2, 3];
+    const [footSelect, setFootSelect] = useState('center');
+    //아이템 클릭할 때 index랑 타입을 넘김 실행 될 때 둘 다 -1로 바꿔줌
+    //레드면 레드인덱스 넣어주고 퍼플이면 퍼블 넣어주고
+
     return (
         <A.CheckQABox>
             <A.CheckQATop>
@@ -16,9 +18,35 @@ const CheckQuestion = () => {
             <A.CheckQACont>
                 <A.CheckQATxt>질문 들어갈 부분</A.CheckQATxt>
                 <A.CheckQABtns>
-                    <A.CheckQABtnList>
-                        <CheckButton />
-                    </A.CheckQABtnList>
+                    {foot.map((list, idx) => (
+                        <A.CheckQABtn
+                            key={idx}
+                            onClick={() => {
+                                console.log(idx);
+                                setFootSelect('left');
+                            }}
+                            type="button"
+                        >
+                            <A.CheckFootL />
+                        </A.CheckQABtn>
+                    ))}
+
+                    <A.CheckQABtn onClick={() => setFootSelect('center')} type="button">
+                        <A.CheckFootCenter />
+                    </A.CheckQABtn>
+
+                    {foot.map((list, idx) => (
+                        <A.CheckQABtn
+                            key={idx}
+                            onClick={() => {
+                                console.log(idx);
+                                setFootSelect('right');
+                            }}
+                            type="button"
+                        >
+                            <A.CheckFootR />
+                        </A.CheckQABtn>
+                    ))}
                 </A.CheckQABtns>
             </A.CheckQACont>
         </A.CheckQABox>
