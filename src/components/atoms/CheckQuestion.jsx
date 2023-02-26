95f382f04e26c8220112395b178ec3d5a87a7fb7
2D0 <lo7733ve@naver.com>
2023-02-26 17:25:43 +0900

    feat: check 문항 컴포넌트 작업 중

--- src/components/atoms/CheckQuestion.jsx
+++ src/components/atoms/CheckQuestion.jsx
@@ -1,6 +1,27 @@
 import React from 'react';
 
+//컴포넌트
+import CheckButton from '@/components/atoms/CheckButton.jsx';
+
+//스타일
+import * as A from '@components/atoms/atoms.style.jsx';
+
 const CheckQuestion = () => {
-    return <></>;
+    return (
+        <A.CheckQABox>
+            <A.CheckQATop>
+                <A.CheckQATitL>아니다</A.CheckQATitL>
+                <A.CheckQATitR>그렇다</A.CheckQATitR>
+            </A.CheckQATop>
+            <A.CheckQACont>
+                <A.CheckQATxt>질문 들어갈 부분</A.CheckQATxt>
+                <A.CheckQABtns>
+                    <A.CheckQABtnList>
+                        <CheckButton />
+                    </A.CheckQABtnList>
+                </A.CheckQABtns>
+            </A.CheckQACont>
+        </A.CheckQABox>
+    );
 };
 export default CheckQuestion;
