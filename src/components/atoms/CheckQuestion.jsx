f2a52ba9be2b8aaff1bfafbdd4baa49d1685aa73
2D0 <lo7733ve@naver.com>
2023-02-28 10:54:56 +0900

    feat: 체크 리스트 더미 배열 데이터 연결 후 map으로 엘리먼트 반복 생성, 스타일드 컴포넌트에 svg import 한 뒤 스타일 주고 스타일드 컴포넌트를 연결했던 것을 스타일드 컴포넌트에서 svg import 지우고 CheckQuestion 에 svg import 하여 다이렉트로 svg 컴포넌트를 사용하는 것으로 변경, 발바닥 클릭시 idx에 맞춰 색상 변경되는 작업

--- src/components/atoms/CheckQuestion.jsx
+++ src/components/atoms/CheckQuestion.jsx
@@ -1,55 +1,73 @@
-import React, { useState } from 'react';
-
+import React, { useEffect, useState } from 'react';
+import questionData from '@/assets/constants/questions.json';
+import { ReactComponent as Foot } from '@/assets/img/icons/ico-foot.svg';
 //스타일
 import * as A from '@components/atoms/atoms.style.jsx';
 
 const CheckQuestion = () => {
-    const foot = [1, 2, 3];
-    const [footSelect, setFootSelect] = useState('center');
-    //아이템 클릭할 때 index랑 타입을 넘김 실행 될 때 둘 다 -1로 바꿔줌
-    //레드면 레드인덱스 넣어주고 퍼플이면 퍼블 넣어주고
+    const footLeft = [3, 2, 1]; //아니다 점수
+    const footRight = [1, 2, 3]; //그렇다 점수
 
+    const [leftIdx, setLeftIdx] = useState(2); //아니다 index
+    const [rightIdx, setRightIdx] = useState(-1); //그렇다 index
+    const [footCenter, setFootCenter] = useState(false); //센터 선택 여부
+    const { question } = questionData;
+
+    const footState = (type, idx) => {
+        //아니다, 그렇다 index 초기화 및 센터 무조건 선택
+        setRightIdx(-1);
+        setLeftIdx(2);
+        setFootCenter(true);
+
+        //아니다, 그렇다 선택할 경우 각 조건에 따라 index 선택한 위치 값으로 변경
+        if (type === 'left') {
+            setLeftIdx(idx);
+        } else if (type === 'right') {
+            setRightIdx(idx);
+        }
+    };
     return (
-        <A.CheckQABox>
-            <A.CheckQATop>
-                <A.CheckQATitL>아니다</A.CheckQATitL>
-                <A.CheckQATitR>그렇다</A.CheckQATitR>
-            </A.CheckQATop>
-            <A.CheckQACont>
-                <A.CheckQATxt>질문 들어갈 부분</A.CheckQATxt>
-                <A.CheckQABtns>
-                    {foot.map((list, idx) => (
-                        <A.CheckQABtn
-                            key={idx}
-                            onClick={() => {
-                                console.log(idx);
-                                setFootSelect('left');
-                            }}
-                            type="button"
-                        >
-                            <A.CheckFootL />
-                        </A.CheckQABtn>
-                    ))}
+        <div>
+            {question.map(item => (
+                <A.CheckQABox key={item.id}>
+                    <A.CheckQATop>
+                        <A.CheckQATitL>아니다</A.CheckQATitL>
+                        <A.CheckQATitR>그렇다</A.CheckQATitR>
+                    </A.CheckQATop>
+                    <A.CheckQACont>
+                        <A.CheckQATxt>질문 들어갈 부분</A.CheckQATxt>
+                        <A.CheckQABtns>
+                            {footLeft.map((list, idx) => (
+                                <A.CheckQABtn key={idx} onClick={() => footState('left', idx - 1)} type="button">
+                                    <A.CheckFootLeft checkState={idx > leftIdx}>
+                                        <Foot />
+                                    </A.CheckFootLeft>
+                                </A.CheckQABtn>
+                            ))}
 
-                    <A.CheckQABtn onClick={() => setFootSelect('center')} type="button">
-                        <A.CheckFootCenter />
-                    </A.CheckQABtn>
+                            <A.CheckQABtn
+                                onClick={() => {
+                                    footState('center');
+                                }}
+                                type="button"
+                            >
+                                <A.CheckFootCenter footCenter={footCenter}>
+                                    <Foot />
+                                </A.CheckFootCenter>
+                            </A.CheckQABtn>
 
-                    {foot.map((list, idx) => (
-                        <A.CheckQABtn
-                            key={idx}
-                            onClick={() => {
-                                console.log(idx);
-                                setFootSelect('right');
-                            }}
-                            type="button"
-                        >
-                            <A.CheckFootR />
-                        </A.CheckQABtn>
-                    ))}
-                </A.CheckQABtns>
-            </A.CheckQACont>
-        </A.CheckQABox>
+                            {footRight.map((list, idx) => (
+                                <A.CheckQABtn key={idx} onClick={() => footState('right', idx + 1)} type="button">
+                                    <A.CheckFootRight checkState={idx < rightIdx}>
+                                        <Foot />
+                                    </A.CheckFootRight>
+                                </A.CheckQABtn>
+                            ))}
+                        </A.CheckQABtns>
+                    </A.CheckQACont>
+                </A.CheckQABox>
+            ))}
+        </div>
     );
 };
 export default CheckQuestion;
