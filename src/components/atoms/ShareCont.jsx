10d0ef390c0bc7e414dae52bd60d236ca0281401
2D0 <lo7733ve@naver.com>
2023-03-01 18:55:15 +0900

    feat: 공유하기 컴포넌트 이름 변경

--- src/components/atoms/ShareCont.jsx
+++ src/components/atoms/ShareCont.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 import * as A from '@/components/atoms/atoms.style.jsx';
 
 //이미지
-import { Share, SnsF, SnsK, SnsL, SnsT } from '@/assets/img/icons/index.js';
+import { Share, SnsFaceBook, SnsKakao, SnsLink, SnsTwitter } from '@/assets/img/icons/index.js';
 
 const ShareCont = () => {
     return (
@@ -16,22 +16,22 @@ const ShareCont = () => {
             <A.CommonSns>
                 <A.CommonSnsIcon>
                     <A.CommonSnsLink to="/">
-                        <A.CommonSnsIconImg src={SnsF} alt="페이스북" />
+                        <A.CommonSnsIconImg src={SnsFaceBook} alt="페이스북" />
                     </A.CommonSnsLink>
                 </A.CommonSnsIcon>
                 <A.CommonSnsIcon>
                     <A.CommonSnsLink to="/">
-                        <A.CommonSnsIconImg src={SnsK} alt="카카오톡" />
+                        <A.CommonSnsIconImg src={SnsKakao} alt="카카오톡" />
                     </A.CommonSnsLink>
                 </A.CommonSnsIcon>
                 <A.CommonSnsIcon>
                     <A.CommonSnsLink to="/">
-                        <A.CommonSnsIconImg src={SnsT} alt="트위터" />
+                        <A.CommonSnsIconImg src={SnsTwitter} alt="트위터" />
                     </A.CommonSnsLink>
                 </A.CommonSnsIcon>
                 <A.CommonSnsIcon>
                     <A.CommonSnsLink to="/">
-                        <A.CommonSnsIconImg src={SnsL} alt="링크" />
+                        <A.CommonSnsIconImg src={SnsLink} alt="링크" />
                     </A.CommonSnsLink>
                 </A.CommonSnsIcon>
             </A.CommonSns>
