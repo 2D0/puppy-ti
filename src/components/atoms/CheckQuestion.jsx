8db655b5c93adba37d8ce54cad928b5a32088d1e
2D0 <lo7733ve@naver.com>
2023-03-01 18:54:27 +0900

    feat: 문항 선택 기능 작업 중

--- src/components/atoms/CheckQuestion.jsx
+++ src/components/atoms/CheckQuestion.jsx
@@ -1,17 +1,17 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import questionData from '@/assets/constants/questions.json';
 import { ReactComponent as Foot } from '@/assets/img/icons/ico-foot.svg';
+
 //스타일
 import * as A from '@components/atoms/atoms.style.jsx';
 
 const CheckQuestion = () => {
-    const footLeft = [3, 2, 1]; //아니다 점수
-    const footRight = [1, 2, 3]; //그렇다 점수
+    const { question } = questionData; //질문 데이터
 
+    const [questionIdx, setQuestionIdx] = useState(0); //질문 index
     const [leftIdx, setLeftIdx] = useState(2); //아니다 index
     const [rightIdx, setRightIdx] = useState(-1); //그렇다 index
     const [footCenter, setFootCenter] = useState(false); //센터 선택 여부
-    const { question } = questionData;
 
     const footState = (type, idx) => {
         //아니다, 그렇다 index 초기화 및 센터 무조건 선택
@@ -30,15 +30,22 @@ const CheckQuestion = () => {
         <div>
             {question.map(item => (
                 <A.CheckQABox key={item.id}>
+                    {questionIdx}
                     <A.CheckQATop>
                         <A.CheckQATitL>아니다</A.CheckQATitL>
                         <A.CheckQATitR>그렇다</A.CheckQATitR>
                     </A.CheckQATop>
                     <A.CheckQACont>
-                        <A.CheckQATxt>질문 들어갈 부분</A.CheckQATxt>
+                        <A.CheckQATxt>질문 들어갈 부분 {item.id}</A.CheckQATxt>
                         <A.CheckQABtns>
-                            {footLeft.map((list, idx) => (
-                                <A.CheckQABtn key={idx} onClick={() => footState('left', idx - 1)} type="button">
+                            {[item.score].map((item, idx) => (
+                                <A.CheckQABtn
+                                    key={item}
+                                    onClick={() => {
+                                        footState('left', idx - 1);
+                                    }}
+                                    type="button"
+                                >
                                     <A.CheckFootLeft checkState={idx > leftIdx}>
                                         <Foot />
                                     </A.CheckFootLeft>
@@ -56,8 +63,15 @@ const CheckQuestion = () => {
                                 </A.CheckFootCenter>
                             </A.CheckQABtn>
 
-                            {footRight.map((list, idx) => (
-                                <A.CheckQABtn key={idx} onClick={() => footState('right', idx + 1)} type="button">
+                            {[item.score].map((item, idx) => (
+                                <A.CheckQABtn
+                                    key={item}
+                                    onClick={() => {
+                                        console.log(`low:${item.low}, middle:${item.middle}`);
+                                        footState('right', idx + 1);
+                                    }}
+                                    type="button"
+                                >
                                     <A.CheckFootRight checkState={idx < rightIdx}>
                                         <Foot />
                                     </A.CheckFootRight>
