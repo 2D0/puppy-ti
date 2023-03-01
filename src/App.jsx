f9076db0db697a2c9caf3fd45df34e9838aab180
2D0 <lo7733ve@naver.com>
2023-03-01 18:55:55 +0900

    feat: 헤더에 있던 스크롤이벤트 APP으로 옮겨서 전체 페이지 공통으로 사용

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
 
 //스타일
 import * as C from '@/style/CommonContents.jsx';
+
+//이미지
+import { LogoWhite } from '@/assets/img/logo';
 
 //컴포넌트
 import { Header, Footer, Background } from '@/components/common';
@@ -10,17 +13,36 @@ import { Main, Check, Result, TeamMember, Inquiry, SourceLicense } from '@/compo
 
 function App() {
     const [bgColor, setBgColor] = useState('purple'); //전제 배경 색상
+    const [scrollHeader, setScrollHeader] = useState(false); //헤더 배경 검정색인지 아닌지
     const [name, setName] = useState(''); //강아지 이름
+    const [pageName, setPageName] = useState(''); //페이지 이름(체크 페이지일 경우 헤더 디자인 다르게 하는 용도)
 
+    //스크롤시 배경 on
+    const handleScroll = () => {
+        const scrollPosition = window.scrollY; //스크롤 위치
+        scrollPosition > 50 ? setScrollHeader(true) : setScrollHeader(false);
+    };
+    //스크롤 이벤트 감지
+    useEffect(() => {
+        handleScroll();
+
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
     return (
         <C.Wrap>
-            <Header bgColor={bgColor} />
+            <Header bgColor={bgColor} pageName={pageName} handleScroll={handleScroll} scrollHeader={scrollHeader} />
 
             {bgColor === 'purple' && <Background />}
 
             <Routes>
                 <Route path="/" element={<Main setName={setName} />}></Route>
-                <Route path="/check" element={<Check setBgColor={setBgColor} />}></Route>
+                <Route
+                    path="/check"
+                    element={<Check setBgColor={setBgColor} setPageName={setPageName} scrollHeader={scrollHeader} />}
+                ></Route>
                 <Route path="/result" element={<Result setBgColor={setBgColor} name={name} />}></Route>
                 <Route path="/team_member" element={<TeamMember />}></Route>
                 <Route path="/inquiry" element={<Inquiry />}></Route>
--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -8,9 +8,8 @@ import * as C from '@/style/CommonContents';
 import { LogoBlack, LogoWhite } from '@/assets/img/logo';
 import { TransEn, TransKo } from '@/assets/img/icons';
 
-const Header = ({ bgColor }) => {
-    const [scrollHeader, setScrollHeader] = useState(false); //헤더 배경 검정색인지 아닌지
-    const [logoColor, setLogoColor] = useState(LogoWhite);
+const Header = ({ bgColor, pageName, scrollHeader }) => {
+    const [logoColor, setLogoColor] = useState(LogoWhite); //로고 컬러 상태
 
     //배경 색상에 따른 로고 색상 변경 스위치
     const switchBg = () => {
@@ -26,27 +25,13 @@ const Header = ({ bgColor }) => {
         }
     };
 
-    //스크롤시 배경 on
-    const handleScroll = () => {
-        const scrollPosition = window.scrollY; //스크롤 위치
-        scrollPosition > 50 ? setScrollHeader(true) : setScrollHeader(false);
-    };
     //배경 색상 변경 감지
     useEffect(() => {
         switchBg();
     }, [bgColor]);
 
-    //스크롤 이벤트 감지
-    useEffect(() => {
-        handleScroll();
-
-        window.addEventListener('scroll', handleScroll);
-        return () => {
-            window.removeEventListener('scroll', handleScroll);
-        };
-    }, []);
     return (
-        <C.HeaderCont scrollHeader={scrollHeader} setScrollHeader={setScrollHeader} bgColor={bgColor}>
+        <C.HeaderCont scrollHeader={scrollHeader} pageName={pageName} bgColor={bgColor}>
             <C.HeaderInner>
                 <C.HeaderLogo>
                     <Link to={'/'}>
