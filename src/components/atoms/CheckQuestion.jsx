d63a0f891d96bc3efe7005fa675d7b37865c792c
2D0 <lo7733ve@naver.com>
2023-03-05 01:46:34 +0900

    feat: 질문 버튼 왼쪽 가운데 오른쪽 아닌 하나로 통합 후 고유 id 값으로 왼쪽 가운데 오른쪽 및 점수 판단할 예정 useEffect로 버튼 누를시 고유 id값을 넘겨 문항에 점수 데이터를 넣으려 했으나 무한 루프 이슈가 생김 이슈를 해결하기 위해 useCallback을 사용하였으나 방법이 미숙하여 제대로 작동하지 않음

--- src/components/atoms/CheckQuestion.jsx
+++ src/components/atoms/CheckQuestion.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import questionData from '@/assets/constants/questions.json';
 import { ReactComponent as Foot } from '@/assets/img/icons/ico-foot.svg';
 
@@ -6,29 +6,63 @@ import { ReactComponent as Foot } from '@/assets/img/icons/ico-foot.svg';
 import * as A from '@components/atoms/atoms.style.jsx';
 
 const CheckQuestion = () => {
-    const leftFoot = [1, 2, 3];
     const [question, setQuestion] = useState(questionData.question); //질문 index
-    const [footCenter, setFootCenter] = useState(false); //센터 선택 여부
-    //const [itemType, setItemType ] = useState('center');
+    const [typeState, setTypeState] = useState('');
+    const [scoreState, setScoreState] = useState(null);
 
-    const idxToScore = {
-        0: 3,
-        1: 2,
-        2: 1,
-    };
+    const stateEffect = useCallback(() => {
+        switch (scoreState) {
+            case 1:
+                setTypeState('no');
+                setScoreState(3);
+                break;
+            case 2:
+                setTypeState('no');
+                setScoreState(2);
+                break;
+            case 3:
+                setTypeState('no');
+                setScoreState(1);
+                break;
+            case 4:
+                setTypeState('center');
+                setScoreState(0);
+                break;
+            case 5:
+                setTypeState('yes');
+                setScoreState(1);
+                break;
+            case 6:
+                setTypeState('yes');
+                setScoreState(2);
+                break;
+            case 7:
+                setTypeState('yes');
+                setScoreState(3);
+                break;
+            default:
+                return false;
+        }
+        setQuestion(
+            question.map(item => {
+                return {
+                    ...item,
+                    state: {
+                        type: typeState,
+                        score: scoreState,
+                    },
+                };
+            }),
+        );
+    }, []);
+    useEffect(() => {
+        stateEffect();
+    }, [scoreState]);
 
-    const scoreToIdx = {
-        3: 0,
-        2: 1,
-        1: 2,
-    };
-
-    const footState = (type, itemIdx, idx) => {
-        //아니다, 그렇다 index 초기화 및 센터 무조건 선택
-        setFootCenter(true);
-        //setItemType(type);
-        //아니다, 그렇다 선택할 경우 각 조건에 따라 index 선택한 위치 값으로 변경
-        setQuestion(question.map(item => (item.id === itemIdx ? { ...item, state: { type, score: idxToScore[idx] } } : item)));
+    const checkState = (itemIdx, scoreId) => {
+        setScoreState(scoreId);
+        console.log(scoreState);
+        //console.log('typeState:', typeState, 'scoreState:', scoreState);
     };
     return (
         <div>
@@ -41,42 +75,18 @@ const CheckQuestion = () => {
                     <A.CheckQACont>
                         <A.CheckQATxt>질문 들어갈 부분 {item.id}</A.CheckQATxt>
                         <A.CheckQABtns>
-                            {leftFoot.map((footItem, idx) => (
+                            {questionData.scoreType.map(score => (
                                 <A.CheckQABtn
-                                    key={footItem}
+                                    key={score.id}
                                     onClick={() => {
-                                        footState('no', itemIdx, idx);
+                                        checkState(itemIdx, score.id);
                                     }}
                                     type="button"
                                 >
-                                    <A.CheckFootLeft checkState={idx >= scoreToIdx[item.state.score]}>
+                                    {score.id}
+                                    <A.CheckFoot checkState={score.id}>
                                         <Foot />
-                                    </A.CheckFootLeft>
-                                </A.CheckQABtn>
-                            ))}
-
-                            <A.CheckQABtn
-                                onClick={() => {
-                                    footState('center');
-                                }}
-                                type="button"
-                            >
-                                <A.CheckFootCenter footCenter={footCenter}>
-                                    <Foot />
-                                </A.CheckFootCenter>
-                            </A.CheckQABtn>
-
-                            {leftFoot.map((footItem, idx) => (
-                                <A.CheckQABtn
-                                    key={footItem}
-                                    onClick={() => {
-                                        footState('yes', itemIdx, idx);
-                                    }}
-                                    type="button"
-                                >
-                                    <A.CheckFootRight checkState={idx <= scoreToIdx[item.state.score]}>
-                                        <Foot />
-                                    </A.CheckFootRight>
+                                    </A.CheckFoot>
                                 </A.CheckQABtn>
                             ))}
                         </A.CheckQABtns>
