4cd21e7230d16c65bcc8ab6be9adc990807360c4
2D0 <lo7733ve@naver.com>
2023-03-27 09:38:34 +0900

    feat: 결과 페이지 작업 및 메인 페이지 간단한 수정

--- src/components/pages/Result.jsx
+++ src/components/pages/Result.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 //컴포넌트
@@ -9,6 +9,8 @@ import * as C from '@/style/CommonContents.jsx';
 import * as S from '@components/result/Result.style.jsx';
 
 const Result = ({ setBgColor, name }) => {
+    const [percent, setPercent] = useState(90); //문항 체크 퍼센트
+
     //기본 배경 보라색 없음
     useEffect(() => {
         setBgColor('noPurple'); //페이지 마운트시 배경 off
@@ -21,6 +23,30 @@ const Result = ({ setBgColor, name }) => {
         <C.Content>
             <S.ResultCont>
                 <h2>{name}</h2>
+                <S.ResultPercentCont>
+                    <S.ResultCharBox>
+                        <S.ResultBoxName></S.ResultBoxName>
+                        <S.ResultBoxChar>
+                            <S.ResultBoxCharImg />
+                        </S.ResultBoxChar>
+                    </S.ResultCharBox>
+                    <S.ResultPercentBox>
+                        <S.ResultPercentList>
+                            <S.ResultPercentInfo>
+                                <S.ResultPercentName>내향형</S.ResultPercentName>
+                                <S.ResultPercentNum>18%</S.ResultPercentNum>
+                            </S.ResultPercentInfo>
+                            <C.PercentBar>
+                                <C.PercentBarFill percent={percent}>
+                                    <C.A11yHidden>20%</C.A11yHidden>
+                                </C.PercentBarFill>
+                            </C.PercentBar>
+                        </S.ResultPercentList>
+                    </S.ResultPercentBox>
+                    <S.ResultTxtBox>
+                        <S.ResultTxt>결과</S.ResultTxt>
+                    </S.ResultTxtBox>
+                </S.ResultPercentCont>
                 <S.RestartBtn>
                     <C.CommonBtn as={Link} to={'/'}>
                         다시하기
