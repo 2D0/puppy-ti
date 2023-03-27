b6591ce7e7e6724301755be72017c1000950b18f
2D0 <lo7733ve@naver.com>
2023-03-27 09:37:49 +0900

    feat: mbti 이미지 내보내기 index

--- src/assets/img/mbti/index.js
+++ src/assets/img/mbti/index.js
@@ -1,1 +1,16 @@
-export { default as Intj } from './intj.png';
+export { default as Istj } from './mbti-istj.png';
+export { default as Istp } from './mbti-istp.png';
+export { default as Isfj } from './mbti-isfj.png';
+export { default as Isfp } from './mbti-isfp.png';
+export { default as Intj } from './mbti-intj.png';
+export { default as Intp } from './mbti-intp.png';
+export { default as Infj } from './mbti-infj.png';
+export { default as Infp } from './mbti-infp.png';
+export { default as Estj } from './mbti-estj.png';
+export { default as Estp } from './mbti-estp.png';
+export { default as Esfj } from './mbti-esfj.png';
+export { default as Esfp } from './mbti-esfp.png';
+export { default as Entj } from './mbti-entj.png';
+export { default as Entp } from './mbti-entp.png';
+export { default as Enfp } from './mbti-enfp.png';
+export { default as Enfj } from './mbti-enfj.png';
