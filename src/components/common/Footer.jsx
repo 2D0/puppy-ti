f0a62a817204bc80f7a69fcd20d09876f52a201b
2D0 <lo7733ve@naver.com>
2023-04-12 21:48:34 +0900

    feat: 푸터 메뉴 이름 변경

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -11,10 +11,10 @@ const Footer = () => {
                     <C.FooterInfoLink to={'/team_member'}>팀원 소개</C.FooterInfoLink>
                 </C.FooterInfoList>
                 <C.FooterInfoList>
-                    <C.FooterInfoLink to={'/inquiry'}>개발 문의</C.FooterInfoLink>
+                    <C.FooterInfoLink to={'/inquiry'}>광고 및 후원 문의</C.FooterInfoLink>
                 </C.FooterInfoList>
                 <C.FooterInfoList>
-                    <C.FooterInfoLink to={'/source_license'}>오픈 소스 라이센스</C.FooterInfoLink>
+                    <C.FooterInfoLink to={'/source_license'}>라이센스</C.FooterInfoLink>
                 </C.FooterInfoList>
             </C.FooterInfo>
             <C.FooterCopy>ⓒ 2023. MAKETREE All rights reserved.</C.FooterCopy>
