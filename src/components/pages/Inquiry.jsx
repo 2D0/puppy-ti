e1cb6551e0a5defa2380b34ebe114a22d4cda555
2D0 <lo7733ve@naver.com>
2023-04-12 21:51:30 +0900

    feat: 문의하기 공통 레이아웃 페이지들

--- src/components/pages/Inquiry.jsx
+++ src/components/pages/Inquiry.jsx
@@ -1,13 +1,26 @@
-import React, { useEffect } from 'react';
+import React, { useState } from 'react';
+
+//컴포넌트
+import NameCover from '@components/atoms/NameCover.jsx';
+import TextBox from '@components/atoms/TextBox.jsx';
+import BackBtn from '@/components/atoms/BackBtn.jsx';
 
 //스타일
 import * as C from '@/style/CommonContents';
-import * as S from '@components/Inquiry/Inquiry.style.jsx';
+import * as S from '@components/inquiry/Inquiry.style.jsx';
 
 const Inquiry = () => {
+    const [pageName, setPageName] = useState('광고 및 후원 문의');
+    const [inquiryData, setInquiryData] = useState(null);
+
     return (
         <C.Content>
-            <S.InquiryCont></S.InquiryCont>
+            <S.InquiryCont>
+                <NameCover pageName={pageName} />
+                <TextBox inquiryData={inquiryData} />
+                <C.CommonBtn type="submit">전송하기</C.CommonBtn>
+                <BackBtn />
+            </S.InquiryCont>
         </C.Content>
     );
 };
