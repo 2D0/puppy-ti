027b6e451f22f59742fa21207249cfe970251631
2D0 <lo7733ve@naver.com>
2023-04-12 21:48:05 +0900

    feat: 문의하기 공통 레이아웃 페이지들

--- src/components/pages/SourceLicense.jsx
+++ src/components/pages/SourceLicense.jsx
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
+import inquiry from '@/assets/constants/inquiry.json';
+
+//컴포넌트
+import NameCover from '@components/atoms/NameCover.jsx';
+import TextBox from '@components/atoms/TextBox.jsx';
+import BackBtn from '@/components/atoms/BackBtn.jsx';
+
+//스타일
+import * as C from '@/style/CommonContents';
+import * as S from '@components/inquiry/Inquiry.style.jsx';
 
 const SourceLicense = () => {
+    const [pageName, setPageName] = useState('오픈 소스 라이센스');
+    const [inquiryData, setInquiryData] = useState(inquiry.sourceLicense);
     return (
-        <div>
-            source license
-        </div>
+        <C.Content>
+            <S.InquiryCont>
+                <NameCover pageName={pageName} />
+                <TextBox inquiryData={inquiryData} />
+                <BackBtn />
+            </S.InquiryCont>
+        </C.Content>
     );
 };
 export default SourceLicense;
--- src/components/pages/TeamMember.jsx
+++ src/components/pages/TeamMember.jsx
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
+import inquiry from '@/assets/constants/inquiry.json';
+
+//컴포넌트
+import NameCover from '@components/atoms/NameCover.jsx';
+import TextBox from '@components/atoms/TextBox.jsx';
+import BackBtn from '@/components/atoms/BackBtn.jsx';
+
+//스타일
+import * as C from '@/style/CommonContents';
+import * as S from '@components/inquiry/Inquiry.style.jsx';
 
 const TeamMember = () => {
+    const [pageName, setPageName] = useState('팀원 소개');
+    const [inquiryData, setInquiryData] = useState(inquiry.teamMember);
     return (
-        <div>
-            teamMember
-        </div>
+        <C.Content>
+            <S.InquiryCont>
+                <NameCover pageName={pageName} />
+                <TextBox inquiryData={inquiryData} />
+                <BackBtn />
+            </S.InquiryCont>
+        </C.Content>
     );
 };
 export default TeamMember;
